373f49de052af7a6f0d99c943d4a44dfdeac0508
yangbiao <yangbiao@shdata.com>
2023-03-27 09:37:12 +0800

    增加百战-秦雷

--- docs/sw.js
+++ docs/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230323.170223';
+const cacheName = 'chirpy-20230327.093350';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
